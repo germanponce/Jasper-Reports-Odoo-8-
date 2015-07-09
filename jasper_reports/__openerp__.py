148a99134019edd49d960f517b4a3ea2195327b3
German <german_442@hotmail.com>
2015-07-09 10:20:52 -0500

    Cambios

--- jasper_reports/__openerp__.py
+++ jasper_reports/__openerp__.py
@@ -35,8 +35,8 @@
     "description" : '''
     This module integrates Jasper Reports with OpenERP. V6 and v7 compatible version was made by NaN-tic.
     Serpent Consulting Services Pvt Ltd has migrated it to v8. ''',
-    "author" : "NaN·tic, Serpent Consulting Services Pvt Ltd",
-    "website" : "http://www.nan-tic.com, http://www.serpentcs.com",
+    "author" : "NaN·tic, Serpent Consulting Services Pvt Ltd, German Ponce Dominguez",
+    "website" : "http://www.nan-tic.com, http://www.serpentcs.com, http://poncesoft.blogspot.com",
     'images' : [
         'images/jasper_reports-hover.png',
         'images/jasper_reports.png'
